95c34aa7d876c8b05c9a7af2e2ca2814de0c6543
BaginBix <timtuvestam@hotmail.se>
2024-12-19 15:17:37 +0100

    Färdigt system

--- Particle.cs
+++ Particle.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -7,20 +8,43 @@ namespace Snow
     {
         private int size;
         private Color color;
-        private float speed = 160;
+        private float minFallSpeed = 10;
+        private float maxFallSpeed = 60;
+        private float fallSpeed = 0;
         private Vector2 position;
         private Texture2D texture;
+        private float time = 0;
+
+        private Vector2 velocity = new Vector2();
+
+        public Vector2 Position{
+            get{return position;}
+        }
+
+        public Vector2 Velocity{
+            set{velocity = value;}
+        }
 
         public Particle(int size, Color color, Vector2 position, Texture2D texture){
             this.size = size;
             this.color = color;
             this.position = position;
             this.texture = texture;
+            Random rand = new Random();
+            time = (float)(rand.NextDouble() * MathF.Tau);
+
+            float fallSpeedDiffrence = maxFallSpeed - minFallSpeed;
+            float sizePercent = size / 20f;
+            fallSpeed = minFallSpeed + (fallSpeedDiffrence * sizePercent);
         }
 
         public void Update(){
             float dt = 1f / 60f;
-            position.Y += speed * dt; 
+            time += dt;
+            velocity.Y = fallSpeed*dt;
+            velocity.X += MathF.Sin(time*2); 
+            
+            position +=  velocity;
         }
 
         public void Draw(SpriteBatch spriteBatch){
